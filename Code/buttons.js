96cab02f1a85d6138618832cab857e0dc00efb3b
Henrique Nunes <hrn2001@gmail.com>
2021-11-30 01:17:28 +0000

    Principles buttons done for now

--- Code/buttons.js
+++ Code/buttons.js
@@ -1,59 +1,43 @@
-/*class Button {
-  constructor(btn, win, bgColor) {
-    this.btn = document.querySelector('#'+btn);
-    this.win = document.getElementById('#'+win);
-    this.showing = false;
-    this.bgColor = bgColor;
-    this.onclick = this.showHideWindow();
-  }
-  showHideWindow(btns) {
-    let show = this.showing;
-    for (let b of btns) {
-      if (b.showing)
-        b.hideWindow();
-    }
+window.onload = function() {
+  const logout = document.querySelector("#btn-logout");
+  logout.addEventListener("click", function() {makeLogout();});
+  const login = document.querySelector("#btn-login");
+  login.addEventListener("click", function() {showHideWindow("#btn-login", "#login-window");});
+  const submit_login = document.querySelector("#submit-login");
+  submit_login.addEventListener("click", function () {makeLogin();});
 
-    if (show) this.showWindow();
-    else      this.hideWindow();
-  }
-  showWindow() {
-    this.btn.style.backgroundColor = this.bgColor;
-    this.win.style.display = "inline-block";
-    this.showing = true;
-  }
-  hideWindow() {
-    this.btn.style.backgroundColor = "rgba(255, 255, 255, 0.0)";
-    this.wd.style.display = "none";
-    this.showing = false;
-  }
+  const help = document.querySelector("#btn-help");
+  help.addEventListener("click", function() {showHideWindow("#btn-help", "#help-window");})
+  const settings = document.querySelector("#btn-settings");
+  settings.addEventListener("click", function() {showHideWindow("#btn-settings", "#settings-window");})
+  const ranking = document.querySelector("#btn-ranking");
+  ranking.addEventListener("click", function() {showHideWindow("#btn-ranking", "#ranking-window");})
 }
 
-window.onload = function() {
-  const btn_login = new Button('btn-login', 'login-window', 'wheat');
-  const btn_logout = new Button('btn-logout', null, 'wheat');
-  const btn_settings = new Button('btn-settings', 'settings-window', 'burlywood');
-  const btn_help = new Button('btn-help', 'help-window', 'burlywood');
-  const btn_ranking = new Button('btn-ranking', 'ranking-window', 'burlywood');
-  const btn_start = new Button('btn-start', null, 'burlywood');
-  const btn_stop = new Button('btn-stop', null, 'burlywood');
-  const buttons = [btn_login, btn_logout, btn_settings, btn_help, btn_ranking, btn_start, btn_stop];
-}*/
-
 function makeLogin() {
-  btnlogin.showWindow();
+  const username = document.querySelector("#login-window input[type=text]");
+  const password = document.querySelector("#login-window input[type=password]");
+
+  showHideWindow("#btn-login", "#login-window");
   const elems_notlog = document.querySelectorAll(".not-logged");
-  const elems_log = document.querySelectorAll(".logged");
+  const elems_log = document.querySelectorAll(".logged:not(#btn-logout, #btn-logout-username)");
+  const btn_logout = document.querySelector("#btn-logout");
+  const btn_logout_username = document.querySelector("#btn-logout-username");
+  btn_logout_username.innerHTML = username.value;
   
-  for (let elem of elems_notlog) {
+  for (let elem of elems_notlog)
     elem.style.display = "none";
-  }
-  
-  for (let elem of elems_log) {
+  for (let elem of elems_log)
     elem.style.display = "block";
-  }
+  btn_logout.style.display = "inline-block";
+  btn_logout_username.style.display = "inline-block";
+
+  username.value = "";
+  password.value = "";
 }
 
 function makeLogout() {
+  hideAllWindows();
   const elems_notlog = document.querySelectorAll(".not-logged");
   const elems_log = document.querySelectorAll(".logged");
   
@@ -66,28 +50,23 @@ function makeLogout() {
 }
 
 function showHideWindow(button, window) {
-  const btn = document.getElementById(button);
-  const win = document.getElementById(window);
-  let hide = getComputedStyle(win).display == "none";
+  const btn = document.querySelector(button);
+  const win = document.querySelector(window);
+  let show = getComputedStyle(win).display == "none";
   
+  hideAllWindows();
 
-  if (show) this.showWindow();
-  else      this.hideWindow();
+  if (show) this.showWindow(btn, win);
 }
 function showWindow(btn, win) {
   btn.style.backgroundColor = "burlywood";
   win.style.display = "inline-block";
 }
-function hideWindow(btn, win) {
-  btn.style.backgroundColor = "rgba(255, 255, 255, 0.0)";
-  win.style.display = "none";
+function hideAllWindows() {
+  const btns = document.querySelectorAll(".buttons button");
+  const wins = document.querySelectorAll(".window");
+  for (let b of btns)
+    b.style.backgroundColor = "rgba(255, 255, 255, 0.0)";
+  for (let w of wins)
+    w.style.display = "none";
 }
-
-  if (getComputedStyle(wd).display == "none") {
-    btn.style.backgroundColor = "burlywood";
-    wd.style.display = "inline-block";
-  }
-  else {
-    btn.style.backgroundColor = "rgba(255, 255, 255, 0.0)";
-    wd.style.display = "none";
-  }

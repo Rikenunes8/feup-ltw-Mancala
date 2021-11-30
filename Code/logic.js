5e271b2ebd025100fc60e581beb66284911a7593
Henrique Nunes <hrn2001@gmail.com>
2021-11-30 10:38:09 +0000

    Game logic

---
+++ Code/logic.js
@@ -0,0 +1,21 @@
+class Hand {
+  constructor(nPits, vPits) {
+    this.pits = []
+    for (let _ = 0; _ < nPits; _++) {
+      this.pits.push(vPits);
+    }
+    this.storage = 0;
+  }
+}
+
+class Game {
+  constructor() {
+    this.pits1 = [4,4,4,4,4,4];
+    this.pits2 = [4,4,4,4,4,4];
+  }
+}
+
+
+window.onload = function() {
+
+}

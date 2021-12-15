71bf39747a3b2afcab724ef9d3fbfcc520384f9d
up201907284 <up201907284@fe.up.pt>
2021-12-15 15:27:02 +0000

    lvl 2 untested. Unconfident.

--- Code/player.js
+++ Code/player.js
@@ -31,10 +31,37 @@ class PlayerAI extends Player {
       return Math.floor(Math.random() * this.board.nPits);
     }
     else if (this.level == 2) {
-      return 0; // TODO: best play in the round
+      
+      let lres = [];
+      for(let i = 0; i < this.board.nPits; i++)
+      {
+        let boardSim = this.board;
+        boardSim.sow(1, i);
+        lres.push(boardSim.store2);
+      }
+      return lres.indexOf(Math.max(lres));
+
     }
     else {
       return 0; // TODO: minimax
     }
   }
+}
+
+/**
+ * Given a board, simulates a turn in a given choice.
+ * @param {Board} b 
+ * @param {*} turn 
+ * @param {*} choice 
+ */
+function simulSow(b, turn, choice)
+{
+  let size = b.holes.length;
+  let ownStr = (turn == 0 ? size/2 : size) - 1;
+  let pass = (turn == 0 ? size : size/2) - 1;
+
+  let i = (turn*(size/2) + choice) % size;
+  let seeds = this.takeAllSeeds(b.holes[i]);
+
+  return (b.store2.score);
 }

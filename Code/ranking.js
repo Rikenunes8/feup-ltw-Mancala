f3b6cc395b09796bc046dfe0d357bf72ba867508
Henrique Nunes <hrn2001@gmail.com>
2022-01-22 19:51:30 +0000

    Local storage

--- Code/ranking.js
+++ Code/ranking.js
@@ -1,21 +1,27 @@
 class Ranking {
-  constructor() {
-    this.ranks = {
-      "ranking": [
-        {"nick": "Easy", "victories": 0, "games": 0},
-        {"nick": "Medium", "victories": 0, "games": 0},
-        {"nick": "Hard", "victories": 0, "games": 0}
-      ]
+  constructor(ranks) {
+    if (ranks) {
+      this.ranks = JSON.parse(ranks);
+    }
+    else {
+      this.ranks = {
+        "ranking": [
+          {"nick": "Easy", "victories": 0, "games": 0},
+          {"nick": "Medium", "victories": 0, "games": 0},
+          {"nick": "Hard", "victories": 0, "games": 0}
+        ]
+      }
     }
   }
 }
 
 class RankingLocal extends Ranking {
   constructor() {
-    super();
+    super(localStorage.getItem('ranks'));
   }
   update(aiLevel, win) { 
     this.ranks.ranking[aiLevel-1].games += 1;
     this.ranks["ranking"][aiLevel-1].victories += win? 1:0;
+    localStorage.setItem('ranks', JSON.stringify(this.ranks));
   }
 }

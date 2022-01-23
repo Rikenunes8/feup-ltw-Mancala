425bac324378ffcefa8fbde9a926ae6875103d32
Henrique Nunes <hrn2001@gmail.com>
2022-01-23 17:03:55 +0000

    Move model.removeGame to update

--- Code/Server/update.js
+++ Code/Server/update.js
@@ -8,11 +8,6 @@ let responses = {};
 
 module.exports.remember = function(response, hash, nick) {
   setHeaders(response, 'sse');
-  if (!model.hasGame(hash)) {
-    console.log("not hash game")
-    endResponseWithError(response, 400, "Invalid game reference")
-    return;
-  }
   if (hash in responses) {
     responses[hash][nick] = response;
   }
@@ -25,7 +20,6 @@ module.exports.forget = function(hash, nick) {
   delete responses[hash][nick];
   if (Object.keys(responses[hash]).length === 0) {
     delete responses[hash];
-    model.removeGame(hash)
   } 
 }
 
@@ -34,9 +28,12 @@ module.exports.update = function(hash) {
   const gameState = model.getGame(hash);
   const game = gameState["game"];
   if (game === undefined) return;
-  if ("winner" in game) ranking.update(gameState);
   for(const response in resps) {
-      resps[response].write('data: '+ JSON.stringify(game) +'\n\n');
+    resps[response].write('data: '+ JSON.stringify(game) +'\n\n');
+  }
+  if ("winner" in game) {
+    ranking.update(gameState);
+    model.removeGame(hash);
   }
 }
 
